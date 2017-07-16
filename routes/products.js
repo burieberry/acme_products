81020acf604f318dfb6373ea3a0344828acc1126
Burcu Noyan <burcunoyan@gmail.com>
2017-07-16 17:47:40 -0400

    add route to products page

--- routes/products.js
+++ routes/products.js
@@ -2,16 +2,18 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-// router.get('/products/:id', function(req, res) {});
-// router.get('/products', function(req, res) {});
-
 router.get('/', function(req, res) {
-  res.render('index', {
+  res.render('index', { topProd: db.getMaxRating().name });
+});
+
+router.get('/products', function(req, res) {
+  res.render('products', {
     products: db.getProducts(),
     topProd: db.getMaxRating().name
   });
 });
 
+// router.get('/products/:id', function(req, res) {});
 // router.post('/products', function(req, res) {});
 // router.delete('/products/:id', function(req, res) {});
 
